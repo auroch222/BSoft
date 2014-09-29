4fb00d58204830dfeaa387dd0fd98ae00c2e8da8
irongeorge <ironmusician@gmail.com>
2014-09-30 00:51:09 +0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-//main server :3
-console.log("Hello World!");
+function App(){
+	this.fs = require('fs'); //File System
+	this.path = require('path'); //For handling and transforming file paths.
+	this.http = require('http').createServer(handler); //HTTP server 'handler' function that runs 
+	this.io = require('socket.io'); //Websockets/socket.io 
+	this.mongo = require('mongodb').MongoClient; //MongoDB client
+	this.configs = JSON.parse("./configs/config.json");
+}
 
+var app = new App();
