92660b655d59ba5e2f6fb03957aada1f913ddfb1
irongeorge <ironmusician@gmail.com>
2014-10-06 17:02:33 +0400

    index.php change

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-	echo "Hello World!";
-	echo "Hello world!";
+	
 ?>
 <!Doctype html>
 <html lang="en">
@@ -8,10 +7,14 @@
     <title>Welcome To BSoft</title>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <link href="../styles/main.css" rel="stylesheet" type="text/css"/>
+    <link href="./css/main.css" rel="stylesheet" type="text/css"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
   </head>
   <body>
-    <h2>Hello From BSoft!<br>For Node.js's Sake!<br>shut the fuck up!</h2>
-
+  	<div id="wall">
+	  	<header>
+	  		dhsaudsudasiydyadyaddgdgdadyasdgadyasdgasy
+	  	</header>
+	</div>
   </body>
 </html>
