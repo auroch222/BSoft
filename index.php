496a2a3c2ad1bef4200c3178028d9007403f22bb
irongeorge <ironmusician@gmail.com>
2014-10-06 16:50:06 +0400

    changed

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 	echo "Hello World!";
-	echo "WheeW!";
 ?>
+<!Doctype html>
+<html lang="en">
+  <head>
+    <title>Welcome To BSoft</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link href="../styles/main.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    <h2>Hello From BSoft!<br>For Node.js's Sake!<br>shut the fuck up!</h2>
+
+  </body>
+</html>
