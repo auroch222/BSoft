341d6051dd757f14ee4c457c258aa3b17940405a
irongeorge <ironmusician@gmail.com>
2014-10-06 17:58:51 +0400

    Add array

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 <?php
-	
+	$bsoft = [
+		'title' => 'Wellcome to Bsoft',
+
+	];
 ?>
 <!Doctype html>
 <html lang="en">
   <head>
-    <title>Welcome To BSoft</title>
+    <title><?php echo isset($bsoft['title']) ? $bsoft['title'] : '';?></title>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <link href="./css/main.css" rel="stylesheet" type="text/css"/>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
   </head>
   <body>
+  <!--Main Wall-->
   	<div id="wall">
+  		<!--Header-->
 	  	<header>
 	  		
 	  	</header>
+	  	<!--END OF HEADER-->
+
+	  	<!--Top Menu Bar-->
 		<div class="topmenubar">
 	        <ul>
 
 	        </ul>
-      	</div><!--topmenubar-->
+      	</div>
+      	<!--End Of Menu Bar-->
 	</div>
+	<!--END OF WALL-->
   </body>
 </html>
